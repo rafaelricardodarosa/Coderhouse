c73e789cb5631417f20eb2641335d0e219aace84
Rafael Rosa <103063877+Cofault@users.noreply.github.com>
2022-04-06 17:31:56 -0500

    Create challenge2_input.py

---
+++ random examples/challenge2_input.py
@@ -0,0 +1,4 @@
+dia = int(input ("Qual dia do seu nascimento?"))
+mes = str(input ("Qual mes do seu nascimento?"))
+ano = int(input ("Qual ano do seu nascimento?"))
+print ("Entao voce nasceu em {} de {} de {}, esta correto?".format(dia,mes,ano))
