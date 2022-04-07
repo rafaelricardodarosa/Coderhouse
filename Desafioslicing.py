354f2e1057b47b62f3f3831f1efb9825625bb5b8
Rafael Rosa <103063877+Cofault@users.noreply.github.com>
2022-04-06 21:00:06 -0500

    Create Desafioslicing.py

---
+++ Desafioslicing.py
@@ -0,0 +1,13 @@
+from tkinter import N
+
+
+nombre_alumno = input("Digite el nombre:")
+nota = input("Digite la nota:")
+materia = input("Digite la materia:")
+
+nombre_volteada = nombre_alumno[::-1]
+nota_volteada = nota[::-1]
+materia_volteada = materia[::-1]
+
+print("{} ha sacado un {} en {}".format(nombre_alumno,nota,materia))
+print("{}, {}, {}".format(materia_volteada,nota_volteada,nombre_volteada))
