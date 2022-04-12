0c91738696fb09bc8360a469dd2496e0c7620886
Rafael Rosa <103063877+Cofault@users.noreply.github.com>
2022-04-12 16:17:22 -0500

    Update desafio1listas.py

--- desafio1listas.py
+++ desafio1listas.py
@@ -3,11 +3,19 @@ Lista2 = ["Bye", "Ciao", "Agur", "Adieu"]
 
 Lista1.append(1234)
 Lista1.append("Hola")
+print(Lista1)
+
 Lista2.append("Adios")
 Lista2.append(1234)
-Lista3 = Lista1.pop()
-Lista4 = Lista2.pop("Adios")
-Lista4 = Lista2.pop("Hola")
-Lista5 = Lista4 + Lista3
+print(Lista2)
 
+Lista1.pop()
+Lista2.pop()
+Lista2.pop(0)
+Lista3 = Lista1
+Lista4 = Lista2
+print(Lista3)
+print(Lista4)
+
+Lista5 = Lista3 + Lista4
 print(Lista5)
