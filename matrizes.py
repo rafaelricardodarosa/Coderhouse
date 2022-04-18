70d297e0f86854f3e8796e78f34665fc23fc83fb
Rafael Rosa <103063877+Cofault@users.noreply.github.com>
2022-04-17 21:09:34 -0500

    Create matrizes.py

---
+++ matrizes.py
@@ -0,0 +1,19 @@
+matriz = [[1, 5, 1, 2], [2, 1, 2, 3], [3, 0, 1, 9], [1, 4, 4, 5]]
+
+matriz[0].pop()
+sum(matriz[0])
+matriz[0].append(sum(matriz[0]))
+
+matriz[1].pop()
+sum(matriz[1])
+matriz[1].append(sum(matriz[1]))
+
+matriz[2].pop()
+sum(matriz[2])
+matriz[2].append(sum(matriz[2]))
+
+matriz[3].pop()
+sum(matriz[3])
+matriz[3].append(sum(matriz[3]))
+
+print(matriz)
