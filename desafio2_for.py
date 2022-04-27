f9a8e370186c22378a6c6441e0f418ec86522aa1
Rafael Rosa <103063877+Cofault@users.noreply.github.com>
2022-04-26 21:35:31 -0500

    a

---
+++ desafio2_for.py
@@ -0,0 +1,4 @@
+paises = ["Canada", "USA", "Mexico", "Australia", "Argentina", "China", "India"]
+
+for indice, nombre in enumerate(paises):
+   print("Soy el pais {} en la posicion {} del ranking.".format(nombre, indice))
