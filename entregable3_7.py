4ddd9feb932fe2421bc68b281bedca43cbfa91ba
Rafael Rosa <103063877+Cofault@users.noreply.github.com>
2022-04-27 17:34:15 -0500

    Update entregable3_7.py

--- entregable3_7.py
+++ entregable3_7.py
@@ -1,2 +1,9 @@
 """ 7) Dadas dos listas, debes generar una tercera con todos los elementos que se 
 repitan en ellas, pero no debe repetirse ningún elemento en la nueva lista:"""
+
+lista_1 = ["h",'o','l','a',' ', 'm','u','n','d','o']
+lista_2 = ["h",'o','l','a',' ', 'l','u','n','a']
+
+lista_3 = lista_1 + lista_2
+
+print(lista_3)
